10e774f75f4f5dcfecc90fdcc651aff49ccd124d
Oscar Arklint Hemmingsen <osca5515@edu.easj.dk>
2022-03-08 13:03:10 +0100

    list-item change

--- app/src/main/java/com/example/mandatoryassignment/FirstFragment.kt
+++ app/src/main/java/com/example/mandatoryassignment/FirstFragment.kt
@@ -5,25 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.mandatoryassignment.databinding.FragmentFirstBinding
+import com.example.mandatoryassignment.models.ResaleItemsViewModel
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
-
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val resaleItemsViewModel: ResaleItemsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
--- app/src/main/java/com/example/mandatoryassignment/MainActivity.kt
+++ app/src/main/java/com/example/mandatoryassignment/MainActivity.kt
@@ -35,6 +35,10 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        resaleItemsViewModel.updateMessageLiveData.observe(this) {message ->
+            Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
