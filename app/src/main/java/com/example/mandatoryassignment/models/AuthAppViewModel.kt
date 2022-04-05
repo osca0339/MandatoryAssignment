e8d3019bbec1c0f18136433a9331f8cc6794a88c
Oscar Arklint Hemmingsen <74705974+osca0339@users.noreply.github.com>
2022-04-05 14:39:59 +0200

    Made AuthViewModel

---
+++ app/src/main/java/com/example/mandatoryassignment/models/AuthAppViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.mandatoryassignment.models
+
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.mandatoryassignment.repository.AuthAppRepository
+import com.google.firebase.auth.FirebaseUser
+
+class AuthAppViewModel: ViewModel() {
+    private val repository = AuthAppRepository(application = Application())
+    private val userLiveData: MutableLiveData<FirebaseUser> = repository.getUserLiveDataMethod()
+    private val loggedOutLiveData: MutableLiveData<Boolean> = repository.getLoggedOutLiveDataMethod()
+
+    fun login(email: String, password: String) {
+        repository.login(email, password)
+    }
+
+    fun register(email: String, password: String) {
+        repository.register(email, password)
+    }
+
+    fun getUserLiveData(): MutableLiveData<FirebaseUser> {
+        return userLiveData
+    }
+
+    fun logOut() {
+        repository.logOut()
+    }
+
+    fun getLoggedOutLiveData(): MutableLiveData<Boolean> {
+        return loggedOutLiveData
+    }
+
+}
